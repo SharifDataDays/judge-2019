60d54fda026c23c369bd73ce87b9aa78383d01ff
cafebazar-server <the.doors.are.locked@gmail.com>
2019-02-01 19:38:01 +0330

    local changes on config judge

--- src/configuration.py
+++ src/configuration.py
@@ -1,19 +1,19 @@
 from enum import Enum
 
-JUDGE_SERVER_HOST = '0.0.0.0'
+JUDGE_SERVER_HOST = 'localhost'
 JUDGE_SERVER_PORT = 9098
 TEST_TIMEOUT_S = 60
 RUN_TIMEOUT_S = 60
 ACCESS_TIMEOUT_S = 5
 
-REPORT_SERVER_HOST = 'localhost'
-REPORT_SERVER_PORT = 8000
+REPORT_SERVER_HOST = 'datadays.ir'
+REPORT_SERVER_PORT = 80
 REPORT_SERVER_PATH = 'dsvfghjsehtdryjfuihlxdfgchjkl/'
 
 JUDGE_MANDATORY_REQUEST_FIELDS = ['team_id', 'phase_id', 'trial_id', 'dataset_number', 'submissions']
 SUBMISSION_MANDATORY_FIELDS = ['question_id', 'question_type', 'submitted_answer']
 
-ANSWERS_FILE_PATH = '/home/mrtaalebi/projects/backend-2019/json_answers.json'
+ANSWERS_FILE_PATH = '/home/datadays/answer.json'
 
 
 class QuestionType(Enum):
