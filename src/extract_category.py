101b95f55bec8f0b8b36375849655040b5f85c6d
Rasoul <rasoul.am1376@gmail.com>
2019-02-09 17:19:42 +0330

    script for cat extraction, first arg is input, second arg is output

---
+++ src/extract_category.py
@@ -0,0 +1,23 @@
+import sys, csv
+
+
+def extract_category():
+    read_file = open(sys.argv[1])
+    csv_read = csv.reader(read_file)
+
+    write_path=sys.argv[2]
+    write_file = open(write_path, mode='w')
+    write_csv = csv.writer(write_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    next(csv_read)
+
+    for row in csv_read:
+        write_csv.writerow(row[3:6])
+    
+
+if __name__ == "__main__":
+    extract_category()
+    
+
+
+    
